d81aea785683dd65acfbd45954f9ee701e638d96
20011363dikshant <chakrayatji222@gmail.com>
2026-02-03 12:09:08 +0530

    update

---
+++ MERN Batch 2/Class13/client/src/Class13/Second Component/secondComponent.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from './secondComponent.module.css'
+
+function SecondComponent(){
+
+
+    return(
+    <div>
+
+        <div className={styles.container}>
+            <h1 className="heading">Hii This is Second Component</h1>
+        </div>
+
+    </div>
+    )
+
+}
+
+export default SecondComponent
