5cb93fdc80655d965535e25df1f950ef36de0ff2
Mansi Sharma <mansi200799@gmail.com>
2026-03-06 09:37:37 +0530

    Update ajio.py

--- Data Engineerring/mansi/flask/router/dynamic/ajio.py
+++ Data Engineerring/mansi/flask/router/dynamic/ajio.py
@@ -1,26 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 
 def fetch_data():
-
-    chrome_options = Options()
-    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-
     driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=chrome_options
+        service=Service(ChromeDriverManager().install())
     )
 
     url = "https://www.ajio.com/search/?text=shirts"
     driver.get(url)
-
     time.sleep(6)
 
     products = driver.find_elements(By.CLASS_NAME, "item")
