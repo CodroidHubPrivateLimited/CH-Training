b00d85d95b728e408394221053be91de27283e1d
Mansi Sharma <mansi200799@gmail.com>
2026-03-06 10:06:16 +0530

    Update ajio.py

--- Data Engineerring/mansi/flask/router/dynamic/ajio.py
+++ Data Engineerring/mansi/flask/router/dynamic/ajio.py
@@ -4,7 +4,6 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-
 def fetch_data():
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install())
