98d8b9f5df97a2bd0df4601ae1abd780d8f1f268
Vishalsachdeva2005 <ch.intern.vishal@gmail.com>
2026-04-02 15:36:54 +0530

    .

--- MERN Batch 2/Class11/BackendManagement/All Routes/route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const {SaveDataController} = require("../All Controllers/userDataSave")
-
-const router= express.Router()
-
-router.post('/firs/seggrigated/post', SaveDataController);
-
-module.exports = router;
--- MERN Batch 2/Class12/BackendManagement/All Routes/route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const {RegisteringUser} = require("../All Controllers/User Registration/registration")
-
-const router= express.Router()
-
-router.post('/firs/seggrigated/post', RegisteringUser);
-
-module.exports = router;
